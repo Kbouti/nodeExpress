ae7334576d30da7acbfe70fca99ecabe80f7f729
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-15 10:10:31 -0700

    Create route for contact me page

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,9 @@ router.get('/about', function(req, res, next) {
   res.render('about', { title: 'Express' });
 });
 
+/* GET contact route */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
 module.exports = router;
